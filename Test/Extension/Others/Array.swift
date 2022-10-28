4993e6d73e74978ed416640bca47156721ca08b5
Milan Savaliya <38990640+MilanSavaliya321@users.noreply.github.com>
2022-10-28 15:40:07 +0530

    Update Array.swift

--- Test/Extension/Others/Array.swift
+++ Test/Extension/Others/Array.swift
@@ -21,6 +21,28 @@ extension Array where Element: Hashable {
 //    }
 }
 
+extension Array {
+    mutating func remove(at indexes: [Int]) {
+        for index in indexes.sorted(by: >) {
+            remove(at: index)
+        }
+    }
+}
+
+extension Array where Element: Hashable {
+    var uniques: Array {
+        var buffer = Array()
+        var added = Set<Element>()
+        for elem in self {
+            if !added.contains(elem) {
+                buffer.append(elem)
+                added.insert(elem)
+            }
+        }
+        return buffer
+    }
+}
+
 //removing Duplicates from info
 extension Sequence where Iterator.Element: Hashable {
     func removingDuplicates() -> [Iterator.Element] {
