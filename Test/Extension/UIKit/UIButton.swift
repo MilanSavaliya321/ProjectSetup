641d152f1e288979ad3e8cd92a9a37cafa0430f7
Milan Savaliya <38990640+MilanSavaliya321@users.noreply.github.com>
2022-10-28 15:32:27 +0530

    Update UIButton.swift

--- Test/Extension/UIKit/UIButton.swift
+++ Test/Extension/UIKit/UIButton.swift
@@ -18,3 +18,33 @@ extension UIButton {
         self.setAttributedTitle(titleString, for: .normal)
     }
 }
+
+extension UIButton {
+    
+    func setImageTintColor(_ color: UIColor) {
+        let tintedImage = self.imageView?.image?.withRenderingMode(.alwaysTemplate)
+        self.setImage(tintedImage, for: .normal)
+        self.tintColor = color
+    }
+    
+    func leftImage(image: UIImage, renderMode: UIImage.RenderingMode) {
+        self.setImage(image.withRenderingMode(renderMode), for: .normal)
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: image.size.width / 2)
+        self.contentHorizontalAlignment = .left
+        self.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    func rightImage(image: UIImage, renderMode: UIImage.RenderingMode){
+        self.setImage(image.withRenderingMode(renderMode), for: .normal)
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left:image.size.width / 2, bottom: 0, right: 0)
+        self.contentHorizontalAlignment = .right
+        self.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    func hasImage(named imageName: String, for state: UIControl.State) -> Bool {
+        guard let buttonImage = image(for: state), let namedImage = UIImage(named: imageName) else {
+            return false
+        }
+        return buttonImage.pngData() == namedImage.pngData()
+    }
+}
