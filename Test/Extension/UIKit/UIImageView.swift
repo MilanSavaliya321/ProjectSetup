23d6b2e73cecab6a603b66f8485bb39832001cd1
Milan Savaliya <38990640+MilanSavaliya321@users.noreply.github.com>
2022-10-28 15:30:38 +0530

    Create UIImageView.swift

---
+++ Test/Extension/UIKit/UIImageView.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImageColor(color: UIColor) {
+        let templateImage = self.image?.withRenderingMode(.alwaysTemplate)
+        self.image = templateImage
+        self.tintColor = color
+    }
+}
